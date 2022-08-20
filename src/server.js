fd5323ef1bea47ff6d94a954f944abd961cd119b
shashifw11 <shashi_fw11_220@masai.school>
2022-08-20 23:52:57 +0530

    first

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const app = require("./index") ; 
+const dotenv = require("dotenv");
 
 const connect = require("./configs/db") ; 
- const PORT = 2345 ; 
-  app.listen( process.env.PORT || PORT , async()=>{
+ const PORT = process.env.PORT || 2345 ; 
+
+  app.listen( PORT , async()=>{
     try{
         await connect() ; 
-        console.log("listning port 2345");
+        console.log(`listning port ${PORT}`);
     }catch(err){
         console.log(err.message);
     }
