32ba0bbe78a7d3ebbf476f40eec7bae510052698
shashifw11 <shashi_fw11_220@masai.school>
2022-08-20 22:43:25 +0530

    make it better

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = require("./index") ;
 
 const connect = require("./configs/db") ; 
  const PORT = 2345 ; 
-  app.listen(process.env.PORT || PORT , async()=>{
+  app.listen( process.env.PORT || PORT , async()=>{
     try{
         await connect() ; 
         console.log("listning port 2345");
