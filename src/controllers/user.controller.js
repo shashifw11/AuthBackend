8d7a97873a48b1d833cef2cfd574ddd8c002e8c3
shashifw11 <shashi_fw11_220@masai.school>
2022-08-22 03:08:10 +0530

    update package.json

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,7 +7,7 @@ const User = require("../models/user.model");
 router.get("/" , async (req,res)=>{
   try {
           const user = await User.find().lean().exec() ; 
-            return res.status(200).send(user);
+            return res.status(200).send({user});
     } catch (error) {
          console.log(error);
     }
