3603ec9deacb8d67802fc12407931b1c7243afe6
shashifw11 <shashi_fw11_220@masai.school>
2022-08-23 12:14:47 +0530

    update cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require("express"); 
-
+const cors = require("cors");
 const userController = require("./controllers/user.controller")
  const { register , login , newToken} = require("./controllers/auth.controller")
 
@@ -7,6 +7,7 @@ const app = express() ;
 
 app.use(express.json()) ; 
 
+app.use(cors());
 
 
 app.post("/register" , register) ; 
